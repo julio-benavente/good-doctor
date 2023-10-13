e4d731e59d4bf3a9a2baada9d0f28f46d4f73c5c
Julio Benavente <julio.benavente.02@gmail.com>
2023-10-13 12:55:11 +0100

    blog section without data fixed

--- src/app/request-an-appointment/page.tsx
+++ src/app/request-an-appointment/page.tsx
@@ -1,14 +1,17 @@
 import { BlogsSection, TestimonialsSlider } from "@/components";
 import FormSection from "./FormSection";
 import CharacteristicsSection from "./CharacteristicsSection";
+import { getBlogsData } from "@/helpers/getBlogsData";
 
-const RequestAnAppointment = () => {
+const RequestAnAppointment = async () => {
+  const blogsData = await getBlogsData({ numberOfItems: 4 });
+
   return (
     <div>
       <FormSection />
       <CharacteristicsSection />
       <TestimonialsSlider theme="secondary" />
-      <BlogsSection />
+      <BlogsSection data={blogsData} />
     </div>
   );
 };
